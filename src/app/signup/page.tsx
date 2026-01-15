caa2bad1f93ea57168a68232e6922b28a0e3d09b
eightsenses <info@eight-senses.jp>
2026-01-15 10:32:26 +0900

    feat: Supabaseのクライアントを追加

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { supabase } from '@/utils/supabase';
+import { useState } from 'react';
+
+import React from 'react';
+
+export default function Signup() {
+  return <div></div>;
+}
