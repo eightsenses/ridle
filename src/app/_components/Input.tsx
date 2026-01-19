854888751f11b44211be997d1b321d38d3c2d962
eightsenses <info@eight-senses.jp>
2026-01-19 16:40:06 +0900

    feat: Footer、Header、Input、Buttonコンポーネントを追加し、機能を実装

--- src/app/_components/Input.tsx
+++ src/app/_components/Input.tsx
@@ -1,7 +1,28 @@
 'use client';
+import { Eye, EyeOff } from 'lucide-react';
+import React, { useState } from 'react';
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
-interface InputProps<T extends FieldValues> {
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+
+const inputVariants = cva(
+  'bg-gray-100 px-4 py-3 text-[16px] leading-normal text-[var(--colors-semantic-text-black)] placeholder:text-[var(--colors-semantic-text-placeholder)] focus:border-[var(--colors-semantic-border-focus)] focus:outline-none',
+  {
+    variants: {
+      size: {
+        default: 'w-full'
+      }
+    },
+    defaultVariants: {
+      size: 'default'
+    }
+  }
+);
+interface InputProps<T extends FieldValues>
+  extends
+    Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>,
+    VariantProps<typeof inputVariants> {
   label: string;
   type: string;
   placeholder?: string;
@@ -18,20 +39,50 @@ const Input = <T extends FieldValues>({
   register,
   placeholder,
   error,
-  isSubmitting = false
+  isSubmitting = false,
+  size,
+  className
 }: InputProps<T>) => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const isPassword = type === 'password';
+  const passwordType = isShowPassword ? 'text' : 'password';
+  const togglePasswordIcon = () => {
+    setIsShowPassword((prev) => !prev);
+  };
+
   return (
     <div>
       <label className="mb-2 block text-[14px] font-medium text-[var(--colors-semantic-text-default)]">
         {label}
       </label>
-      <input
-        className="w-full rounded-md border border-[var(--colors-semantic-border-default)] bg-white px-4 py-3 text-[16px] leading-normal text-[var(--colors-semantic-text-black)] placeholder:text-[var(--colors-semantic-text-placeholder)] focus:border-[var(--colors-semantic-border-focus)] focus:outline-none"
-        type={type}
-        placeholder={placeholder}
-        {...register(field)}
-        disabled={isSubmitting}
-      />
+      {!isPassword ? (
+        <input
+          className={cn(inputVariants({ size, className }))}
+          type={type}
+          placeholder={placeholder}
+          {...register(field)}
+          disabled={isSubmitting}
+        />
+      ) : (
+        <div className="relative">
+          <input
+            className={cn(inputVariants({ size, className }))}
+            type={passwordType}
+            placeholder={placeholder}
+            {...register(field)}
+            disabled={isSubmitting}
+          />
+
+          <button
+            type="button"
+            onClick={togglePasswordIcon}
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+            aria-label="Toggle password visibility"
+          >
+            {isShowPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+        </div>
+      )}
       {error && <p className="mt-1 text-[14px] text-red-700">{error}</p>}
     </div>
   );
