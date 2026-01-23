1aa870c68b5cbdc160dc9be27b5cce8d0695aeea
eightsenses <info@eight-senses.jp>
2026-01-24 01:24:57 +0900

    feat: お問い合わせフォームの実装とメール送信機能の追加

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { Resend } from 'resend';
+import MailTemplate from '@/app/contact/_components/MailTemplate';
+import { createElement } from 'react';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.RESEND_FROM_EMAIL as string;
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const req = await request.json();
+    const { data, error } = await resend.emails.send({
+      from: fromEmail,
+      to: [req.email],
+      bcc: fromEmail,
+      subject: 'お問い合わせありがとうございます',
+      react: createElement(MailTemplate, { senderName: req.name, content: req.message })
+    });
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+};
