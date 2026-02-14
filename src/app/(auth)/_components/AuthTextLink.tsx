4383087a698d1129ead563e2db596de615144707
eightsenses <info@eight-senses.jp>
2026-02-14 10:35:09 +0900

    fix: twMergeを使用してAuthTextLinkコンポーネントのクラス名を統一

--- src/app/(auth)/_components/AuthTextLink.tsx
+++ src/app/(auth)/_components/AuthTextLink.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { ComponentProps, FC } from 'react';
 import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
 
 type Props = ComponentProps<typeof Link>;
 
-const AuthTextLink: FC<Props> = ({ className = '', ...props }) => {
-  return <Link {...props} className={`text-semantic-text-primary hover:underline ${className}`} />;
+const AuthTextLink: FC<Props> = ({ className, ...props }) => {
+  return (
+    <Link {...props} className={twMerge('text-semantic-text-primary hover:underline', className)} />
+  );
 };
 export default AuthTextLink;
