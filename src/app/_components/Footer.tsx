332eb397e9af2abdd9b64c9325c0895d744a71e5
eightsenses <info@eight-senses.jp>
2026-03-04 18:09:59 +0900

    feat: プライバシーポリシーと利用規約ページを追加

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { FC } from 'react';
 import Link from 'next/link';
-import X from '@/assets/svg/x-icon.svg';
+// import X from '@/assets/svg/x-icon.svg';
 import Insta from '@/assets/svg/insta-icon.svg';
 
 const Footer: FC = () => {
@@ -17,16 +17,30 @@ const Footer: FC = () => {
         &copy;{year} Ridle
       </p>
       <div className="flex shrink-0 flex-col items-center gap-4 md:flex-row md:gap-6">
-        <Link
-          href="/contact"
-          className="text-right text-[14px] font-normal leading-none text-semantic-text-default hover:underline"
-        >
-          お問い合わせ
-        </Link>
         <div className="flex shrink-0 items-center gap-3">
-          <a href={socialUrls.x} aria-label="Xページ" target="_blank">
+          <Link
+            href="/terms"
+            className="text-right text-xs font-normal leading-none text-semantic-text-default hover:underline md:text-sm"
+          >
+            利用規約
+          </Link>
+          <Link
+            href="/privacy"
+            className="text-right text-xs font-normal leading-none text-semantic-text-default hover:underline md:text-sm"
+          >
+            プライバシーポリシー
+          </Link>
+          <Link
+            href="/contact"
+            className="text-right text-xs font-normal leading-none text-semantic-text-default hover:underline md:text-sm"
+          >
+            お問い合わせ
+          </Link>
+        </div>
+        <div className="flex shrink-0 items-center gap-3">
+          {/* <a href={socialUrls.x} aria-label="Xページ" target="_blank">
             <X className="h-4 w-4 transition-opacity duration-300 hover:opacity-70" />
-          </a>
+          </a> */}
           <a href={socialUrls.insta} aria-label="Instagramページ" target="_blank">
             <Insta className="rtansition-opacity h-4 w-4 duration-300 hover:opacity-70" />
           </a>
